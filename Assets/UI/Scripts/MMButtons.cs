200b7b3ed8df078d9a0494c47288c2879af8bfe3
Jeremiah Moore <jay.moore@trojans.dsu.edu>
2025-02-13 10:55:28 -0600

    Main Menu and Level 1

---
+++ Assets/UI/Scripts/MMButtons.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MMButtons : MonoBehaviour
+{
+    public string GameScene;
+
+    private UIDocument mainMenu;
+    private Button startButton;
+    private Button exitButton;
+
+    private void OnEnable()
+    {
+        mainMenu = GetComponent<UIDocument>();
+
+        startButton = mainMenu.rootVisualElement.Q("StartButton") as Button;
+        exitButton = mainMenu.rootVisualElement.Q("ExitButton") as Button;
+
+        startButton.RegisterCallback<ClickEvent>(StartGame);
+        exitButton.RegisterCallback<ClickEvent>(ExitGame);
+    }
+
+    private void OnDisable()
+    {
+        startButton.UnregisterCallback<ClickEvent>(StartGame);
+        exitButton.UnregisterCallback<ClickEvent>(ExitGame);
+    }
+
+    private void StartGame(ClickEvent evt)
+    {
+        SceneManager.LoadScene(GameScene);
+    }
+
+    private void ExitGame(ClickEvent evt)
+    {
+        Debug.Log("success");
+        Application.Quit();
+    }
+}
