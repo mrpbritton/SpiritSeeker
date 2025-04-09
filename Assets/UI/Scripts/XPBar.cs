4582effaecbd7abdaec79895aea2ab4fd28751c9
Jeremiah Moore <124409490+JuicyCitrus@users.noreply.github.com>
2025-04-09 11:48:46 -0500

    Level Up Stuff Started

---
+++ Assets/UI/Scripts/XPBar.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class XPBar : MonoBehaviour
+{
+    // UI Doc Related Values
+    private UIDocument playerHUD;
+    private VisualElement experienceBar;
+    private float xPBarWidth;
+    private bool baseXPFound = false;
+    public float XPFill = 1;
+
+    private float currentXp = 0;
+    public float maxXp = 100;
+
+    private void OnEnable()
+    {
+        playerHUD = GetComponent<UIDocument>();
+        experienceBar = playerHUD.rootVisualElement.Q("XPFill") as VisualElement;
+    }
+
+    private void Update()
+    {
+        if (experienceBar.resolvedStyle.width >= 0 && !baseXPFound)
+        {
+            baseXPFound = true;
+            xPBarWidth = experienceBar.resolvedStyle.width;
+            UpdateXPBar(0, maxXp);
+        }
+    }
+
+    public void XPGained(float amount)
+    {
+        currentXp += amount;
+        UpdateXPBar(currentXp, maxXp);
+    }
+
+    public void UpdateXPBar(float currentXP, float maxXP)
+    {
+        if (experienceBar.resolvedStyle.width >= 0)
+        {
+            XPFill = Mathf.Clamp(currentXP / maxXP, 0, 1);
+            experienceBar.style.width = xPBarWidth * XPFill;
+        }
+    }
+}
